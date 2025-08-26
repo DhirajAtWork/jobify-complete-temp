146ac5a414945dfb5a1e19e49d0002e454ea5ddd
Dhiraj Atal <dheerajatal3806@gmail.com>
2025-08-26 17:20:32 +0530

    frontend fixed 2.0

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,9 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
 import Job from "../models/JobModel.js";
 import cloudinary from "cloudinary";
-import { promises as fs } from "fs";
 import { response } from "express";
+
+import { formatImage } from "../middleware/multerMiddleware.js";
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
@@ -18,23 +19,18 @@ export const getApplicationStats = async (req, res) => {
 };
 
 export const updateUser = async (req, res) => {
-  console.log(req.file);
-
   const newUser = { ...req.body };
   delete newUser.password;
-
   if (req.file) {
-    const reponse = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
-    newUser.avatar = reponse.secure_url;
+    const file = formatImage(req.file);
+    const response = await cloudinary.v2.uploader.upload(file);
+    newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
-
   const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser);
 
   if (req.file && updatedUser.avatarPublicId) {
     await cloudinary.v2.uploader.destroy(updatedUser.avatarPublicId);
   }
-
-  res.status(StatusCodes.OK).json({ msg: "Updated User", updatedUser });
+  res.status(StatusCodes.OK).json({ msg: "update user" });
 };
