d0e013df04f3734272229db798ce0dd4730b7692
Dhiraj Atal <dheerajatal3806@gmail.com>
2025-08-26 23:42:06 +0530

    final one 3.0

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ cloudinary.config({
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.resolve(__dirname, "/public")));
+app.use(express.static(path.resolve(__dirname, "client/dist")));
 
 // Not Found Middleware!
 
@@ -46,14 +46,6 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.get("/api/v1/test", (req, res) => {
-  res.json({ msg: "test route" });
-});
-
 // Creating Express Router to handle these routes!
 
 // not found and error should be the last used middleware
@@ -63,7 +55,7 @@ app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client/dist", "index.html"));
 });
 
 app.use("*", (req, res) => {
